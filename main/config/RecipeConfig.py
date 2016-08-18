f7e53a31d97ed726f53e2765c5373df07b88a6bd
Floréal Cabanettes <floreal.cab@gmail.com>
2016-08-19 00:31:20 +0200

    Move translations of recipe components to django translator

--- main/config/RecipeConfig.py
+++ main/config/RecipeConfig.py
@@ -1,33 +1,19 @@
-class RecipeConfigGlobal:
+from django.utils.translation import ugettext as _
+
+
+class RecipeConfig:
     directory_photos = "/Photos/"
     photo_in_recipe_width = "254px"
-
-
-class RecipeConfigEn (RecipeConfigGlobal):
-    ingredients_short = "Ingredients (for %d people):"
-    ingredients_short_1p = "Ingredients (for %d person):"
-    ingredients_long = "Ingredients (for %d people (%s)):"
-    ingredients_long_1p = "Ingredients (for %d person (%s)):"
-    ingredients_range_short = "Ingredients (from %d to %d people):"
-    ingredients_range_long = "Ingredients (from %d to %d people (%s)):"
-    equipment = "Required equipment:"
-    instructions = "Instructions:"
-    proposals = "Proposals:"
-    timePreparation = "Prep:"
-    timeCuis = "Cook:"
-    timeRep = "Break:"
-
-
-class RecipeConfigFr (RecipeConfigEn):
-    ingredients_short = "Ingrédients (pour %d personnes)&#8239;:"
-    ingredients_short_1p = "Ingrédients (pour %d personne)&#8239;:"
-    ingredients_long = "Ingrédients (pour %d personnes (%s))&#8239;:"
-    ingredients_long_1p = "Ingrédients (pour %d personne (%s))&#8239;:"
-    ingredients_range_short = "Ingrédients (de %d à %d personnes)&#8239;:"
-    ingredients_range_long = "Ingrédients (de %d à %d personnes (%s))&#8239;:"
-    equipment = "Matériel nécessaire&#8239;:"
-    instructions = "Préparation&#8239;:"
-    proposals = "Conseils&#8239;:"
-    timePreparation = "Temps de préparation&#8239;:"
-    timeCuis = "Temps de cuisson&#8239;:"
-    timeRep = "Temps de repos&#8239;:"
+    # Translators: terms used in a recipe
+    ingredients_short = _("Ingredients (for %d people):")
+    ingredients_short_1p = _("Ingredients (for %d person):")
+    ingredients_long = _("Ingredients (for %d people (%s)):")
+    ingredients_long_1p = _("Ingredients (for %d person (%s)):")
+    ingredients_range_short = _("Ingredients (from %d to %d people):")
+    ingredients_range_long = _("Ingredients (from %d to %d people (%s)):")
+    equipment = _("Required equipment:")
+    instructions = _("Instructions:")
+    proposals = _("Proposals:")
+    timePreparation = _("Prep:")
+    timeCuis = _("Cook:")
+    timeRep = _("Break:")
--- main/config/__init__.py
+++ main/config/__init__.py
@@ -2,7 +2,4 @@ import re
 
 from pywebcooking import settings
 
-if re.match('^fr-\w+$', settings.LANGUAGE_CODE):
-    from .RecipeConfig import RecipeConfigFr as RecipeConfig
-else:  # Default is english
-    from .RecipeConfig import RecipeConfigEn as RecipeConfig
+from .RecipeConfig import RecipeConfig
