9e0cc6af260a44f2f3b4a3cfddca4844e72d1326
Floréal Cabanettes <floreal.cab@gmail.com>
2016-08-22 00:39:34 +0200

    Add pseudo to user

--- main/models/User.py
+++ main/models/User.py
@@ -5,6 +5,7 @@ from .Group import Group
 class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
     email = models.CharField(max_length=255)
     date_created = models.DateTimeField('date published', auto_now=True)
     date_last_connection = models.DateTimeField('date published', auto_now=True)
