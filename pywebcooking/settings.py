49f2a3773bb8a42c321f39053438a9001476d568
Floréal Cabanettes <floreal.cab@gmail.com>
2016-08-22 00:41:57 +0200

    Add footer

--- pywebcooking/settings.py
+++ pywebcooking/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'copyright',
     'main',
     'website'
 )
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'website.context_processors.site_params'
             ],
         },
     },
@@ -99,3 +101,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Website parameters:
+
+SITE_NAME = "PyWebCooking"
+
+COPY_START_YEAR = 2012
