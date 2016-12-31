3c745da39e650408c96807970681ea5a7264c605
Floréal Cabanettes <floreal.cab@gmail.com>
2016-12-31 16:11:27 +0100

    Add basic authentification in addition to session authentification and make authentication default to True

--- pywebcooking/settings.py
+++ pywebcooking/settings.py
@@ -58,6 +58,10 @@ MIDDLEWARE_CLASSES = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
