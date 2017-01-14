1b658956a04d647d5263f855a7962f443bf46423
Floréal Cabanettes <floreal.cab@gmail.com>
2017-01-14 22:58:45 +0100

    Move Photos in /media folder

--- pywebcooking/urls.py
+++ pywebcooking/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import patterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
     url(r'^', include('website.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(url(r'^media/(?P<path>.*)',
+                           'django.views.static.serve',
+                           {'document_root': settings.BASE_DIR + settings.MEDIA_ROOT}))
