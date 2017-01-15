4403a56e75eba89aa38749718df9d4d33f12ed6c
Floréal Cabanettes <floreal.cab@gmail.com>
2017-01-15 17:01:03 +0100

    Make url of UserProfile unique

--- main/models/UserProfile.py
+++ main/models/UserProfile.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, primary_key=True)
-    url = models.CharField(max_length=100)
+    url = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.user.username
