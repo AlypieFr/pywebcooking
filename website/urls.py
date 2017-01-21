b300742805e2bc9ff55f8b688dd81e8f177468a2
Floréal Cabanettes <floreal.cab@gmail.com>
2017-01-21 20:50:24 +0100

    Add favicon

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
+from django.utils.translation import ugettext as _
+from django.views.generic.base import RedirectView
 
 from .views import IndexView, RecipeView
 
-from django.utils.translation import ugettext as _
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     # Translators: recipe is the parent tag of a recipe
     url(r'^' + _("recipe") + '/(?P<slug>\w+)$', RecipeView.as_view(), name='recipe'),
+    url(r'^favicon\.ico$', favicon_view),
 ]
