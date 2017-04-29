d5f130d59cee9dcc9ed2c39b61e69851813a70b6
Floréal Cabanettes <floreal.cab@flo-art.fr>
2017-04-29 18:31:01 +0200

    Order recipes by pub date (DESC) in API index

--- main/controllers/C_Recipe.py
+++ main/controllers/C_Recipe.py
@@ -217,7 +217,7 @@ class CRecipe:
 
     @staticmethod
     def get_author_recipes_data(author: User) -> list:
-        recipes = Recipe.objects.filter(author__user=author)
+        recipes = Recipe.objects.filter(author__user=author).order_by("pub_date").reverse()
         recipes_data = []
         for recipe in recipes:
             recipes_data.append(CRecipe.get_recipe_data(recipe))
