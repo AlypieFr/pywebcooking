f4b12e334b45dc43fffdc791613e3931c9f3ccb5
Floréal Cabanettes <floreal.cab@flo-art.fr>
2017-10-23 10:45:12 +0200

    Fix logout with wsgi

--- website/views/LogoutView.py
+++ website/views/LogoutView.py
@@ -1,12 +1,14 @@
 from django.views.generic import View
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.middleware import csrf
 
 
 class LogoutView(View):
 
     def get(self, request):
         logout(request)
+        csrf.rotate_token(request)
         if request.META["HTTP_REFERER"].find("/panel/") > -1 or request.META["HTTP_REFERER"].find("/admin/") > -1 or \
                 request.META["HTTP_REFERER"].find("/api/") > -1:
             return redirect("/")
